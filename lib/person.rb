3b9c8428bf197e7171384706f77a5216e3eb0626
osagie obaretin <osagie.codes@gmail.com>
2020-06-06 21:40:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,8 @@
-class Person 
-  def talk  
-    puts "Hello World!"  
-  end 
-  def walk
-    puts "The person is walking"
+class Person
+  def talk
+    puts "Hello World!"
   end
-end  
+  def walk
+    puts "The Person is walking"
+  end
+end
